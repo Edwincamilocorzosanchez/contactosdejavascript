098ebdf4d3b4cbfa7f589acfa42d38662fc02cf0
TEstebanGQ <217262372+TEstebanGQ@users.noreply.github.com>
2025-10-02 11:57:58 -0500

    feat: :sparkles: ayuda

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ import '/App/Components/countries/countryComponent.js';
 import '/App/Components/regions/regionComponent.js';
 import '/App/Components/cities/citiesComponents.js';
 import '/App/Components/companies/companyComponent.js';
-import '/App/Components/branches/brancComponenet.js';
+import '/App/Components/branches/branchComponent.js';
